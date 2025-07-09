dc429787bf36c6e9c54acd7ae1a4173750cd5f9c
NickFGit <frync@icloud.com>
2025-07-08 18:05:13 -0600

    create 3 additional game pieces; Door, King, Room

---
+++ src/levelPieces/Door.java
@@ -0,0 +1,19 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Door extends GamePiece {
+
+	public Door(char symbol, String label, int location) {
+		super(symbol, label, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/levelPieces/Room.java
@@ -0,0 +1,19 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Room extends GamePiece {
+
+	public Room(char symbol, String label, int location) {
+		super(symbol, label, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
