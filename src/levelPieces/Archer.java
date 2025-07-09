1916961893cf4eb6e6f1fef09a9f02ec949204ac
NickFGit <frync@icloud.com>
2025-07-08 18:00:13 -0600

    add Archer and Guard that implement moveable

---
+++ src/levelPieces/Archer.java
@@ -0,0 +1,26 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+public class Archer extends GamePiece implements Moveable {
+
+	public Archer(char symbol, String label, int location) {
+		super(symbol, label, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
