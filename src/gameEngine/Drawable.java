af867e7d243b67d27208e5daaf385360da861591
Wesley Montgomery <wesley_montgomery@mines.edu>
2025-07-11 17:02:33 -0600

    Update Drawable.java
    Updated comments.

--- src/gameEngine/Drawable.java
+++ src/gameEngine/Drawable.java
@@ -4,8 +4,8 @@ package gameEngine;
  * All game pieces may be "drawn" - just printed on console, no parameters
  * needed.
  * 
- * @author Mark Baldwin
- * @author Cyndi Rader
+ * @author Nick Fry
+ * @author Wesley Montgomery
  * 
  */
 public interface Drawable {
